46cca03b2c3b1b6e3d1dff821a519ed53f055157
Les Dyman <lesdyman@gmail.com>
2024-10-21 21:14:04 +0200

    add sidebar

---
+++ main.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(".navigation__buttons-button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
