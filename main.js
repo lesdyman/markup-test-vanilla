5aba2e45a3dc05874a04b529329c9dc26febd8bf
Les Dyman <lesdyman@gmail.com>
2024-10-23 20:52:11 +0200

    add preliminary solution

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/**
+ * navigation buttons listener
+ */
+
 const buttons = document.querySelectorAll(".navigation__buttons-button");
 
 buttons.forEach((button) => {
@@ -6,3 +10,37 @@ buttons.forEach((button) => {
     this.classList.add("active");
   });
 });
+
+/**
+ * pagination buttons listener
+ */
+
+const pagination = document.querySelectorAll(
+  ".all-customers__bottom-pagination-button"
+);
+
+pagination.forEach((p_button) => {
+  p_button.addEventListener("click", function () {
+    pagination.forEach((btn) => btn.classList.remove("selected"));
+    this.classList.add("selected");
+  });
+});
+
+/**
+ * Burger menu listener
+ */
+const burgerMenu = document.getElementById("burger-menu");
+const asideNav = document.getElementById("navigation");
+const closeNavBtn = document.getElementById("nav-close");
+
+if (burgerMenu && asideNav) {
+  burgerMenu.addEventListener("click", () => {
+    asideNav.classList.add("open");
+  });
+}
+
+if (closeNavBtn && asideNav) {
+  closeNavBtn.addEventListener("click", () => {
+    asideNav.classList.remove("open");
+  });
+}
